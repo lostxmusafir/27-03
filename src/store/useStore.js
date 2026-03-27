78c04e28bc7eb4015f8a2bc60ad8b342cdc1cff4
raj patil <rajagamer8@gmail.com>
2026-03-27 12:56:26 +0530

    Add activeConvoys state to Zustand store

--- src/store/useStore.js
+++ src/store/useStore.js
@@ -3,19 +3,32 @@ import { create } from 'zustand';
 const useStore = create((set) => ({
   selectedCamp: null,
   setSelectedCamp: (camp) => set({ selectedCamp: camp }),
-  
+
   showAddCampForm: false,
   setShowAddCampForm: (show) => set({ showAddCampForm: show }),
-  
+
   mapClickCoords: null,
   setMapClickCoords: (coords) => set({ mapClickCoords: coords }),
-  
+
   toasts: [],
   addToast: (toast) => set((state) => ({
     toasts: [...state.toasts, { ...toast, id: Date.now() }]
   })),
   removeToast: (id) => set((state) => ({
     toasts: state.toasts.filter(t => t.id !== id)
+  })),
+
+  activeConvoys: [],
+  addActiveConvoy: (convoy) => set((state) => ({
+    activeConvoys: [...state.activeConvoys, convoy]
+  })),
+  updateConvoyProgress: (id, progress) => set((state) => ({
+    activeConvoys: state.activeConvoys.map((c) =>
+      c.id === id ? { ...c, progress } : c
+    )
+  })),
+  removeActiveConvoy: (id) => set((state) => ({
+    activeConvoys: state.activeConvoys.filter((c) => c.id !== id)
   })),
 }));
 
